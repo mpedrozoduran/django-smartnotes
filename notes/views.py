510831739a806075036ea35f1006265d33798fe4
mpedrozoduran <mpedrozoduran@gmail.com>
2023-11-19 18:02:04 -0500

    initial commit

---
+++ notes/views.py
@@ -0,0 +1,46 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
+from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
+
+from .forms import NotesForm
+from .models import Notes
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    login_url = "/login"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = "/smart/notes"
+    template_name = "notes/notes_delete.html"
